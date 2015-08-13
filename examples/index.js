4330c36a044b2734c318c76e3ebd6ffd0b9f3655
Matt Styles <matt@veryfizzyjelly.com>
2015-08-13 08:39:20 +0100

    #update prep for module release

---
+++ examples/index.js
@@ -0,0 +1,189 @@
+
+import random from 'lodash.random'
+
+import CONSTANTS from './constants'
+// import Gui from './gui'
+import HeightMap from './heightMap'
+import MapRender from './mapRender'
+import Simplex from './simplex'
+
+import { radial } from './generators'
+import { Point, Vector2, max, min, euclidean, clamp } from './util'
+
+const renderer = new MapRender({
+    style: {
+        zIndex: 10
+    }
+})
+
+const renderer2 = new MapRender({
+    style: {
+        left: CONSTANTS.WIDTH / window.devicePixelRatio + 'px',
+        zIndex: 10
+    }
+})
+
+function lerp( value, min, max ) {
+    return min + value * ( max - min )
+}
+
+
+
+console.log( 'generating simplex' )
+let start = performance.now()
+
+// Simplex functions
+const simplex = new Simplex({
+    min: 0,
+    max: 1,
+    octaves: 4,
+    persistence: .3,
+    frequency: .01
+})
+const perturb = new Simplex({
+    min: -1,
+    max: 1,
+    persistence: .5,
+    frequency: .005
+})
+
+
+// Heightmap function based on base simplex
+const base = new HeightMap()
+    .addFunction({
+        weight: 1,
+        fn: simplex.getValue
+    })
+
+// Maps x, y to 0-.3
+// LEFT base chunk type
+const baseMuted = new HeightMap()
+    .addFunction({
+        weight: 1,
+        fn: ( x, y ) => base.getValue( x, y ) * .3
+    })
+
+
+// RIGHT base chunk type
+const ridged = new HeightMap()
+    .addFunction({
+        weight: 1,
+        fn: base.getValue
+    })
+    .addFunction({
+        weight: 5,
+        fn: function( x, y ) {
+            // Adds the ridges
+            return Math.abs( perturb.getValue( x, y ) )
+        }
+    })
+    .addFunction({
+        weight: 5,
+        fn: radial( CONSTANTS.WIDTH / 2 )
+    })
+
+
+const right = new HeightMap()
+    .addFunction({
+        weight: 1,
+        fn: function( x, y ) {
+            // Now handle the seam
+            let seamWidth = CONSTANTS.WIDTH / Math.pow( 2, 2 )
+            let pow = 2
+            let val = 0
+            let i = x % CONSTANTS.WIDTH
+
+            let mapValue = ridged.getValue( x, y )
+
+            if ( i < seamWidth ) {
+                // Heightmap is the chunk to the left in this example
+                val = lerp(
+                    Math.pow( i / seamWidth, pow ),
+                    // i / seamWidth,
+                    baseMuted.getValue( x, y ),      // Values from the left chunk
+                    mapValue                    // mixed with values from this chunk
+                )
+
+                return val
+            }
+
+            return mapValue
+        }
+    })
+
+const left = new HeightMap()
+    .addFunction({
+        weight: 1,
+        fn: function( x, y ) {
+            // Now handle the seam
+            let seamWidth = CONSTANTS.WIDTH / Math.pow( 2, 2 )
+            let pow = 2
+            let val = 0
+            let i = x % CONSTANTS.WIDTH
+
+            let mapValue = baseMuted.getValue( x, y )
+
+            if ( i > CONSTANTS.WIDTH - seamWidth ) {
+                // Heightmap is the chunk to the left in this example
+                val = lerp(
+                    Math.pow( i / CONSTANTS.WIDTH - seamWidth, pow ),
+                    // i / seamWidth,
+                    ridged.getValue( x, y ),      // Values from the left chunk
+                    mapValue                    // mixed with values from this chunk
+                )
+
+                return val
+            }
+
+            return mapValue
+        }
+    })
+
+
+// @TODO sort out the seaming functions
+// should also seam on both sides of the seam, i.e. left chunk permeates into
+// right whilst right goes into left, effectively means that x=1 or right chunk
+// is 50:50, not 100:0 as the current right HeightMap lerp creates
+
+
+
+
+console.log( 'done', performance.now() - start )
+
+
+function render() {
+    console.log( 'rendering' )
+    start = performance.now()
+    renderer.clear()
+
+    renderer.render({
+        heightmap: left
+    })
+
+    renderer2.render({
+        heightmap: right,
+        x: 512,
+        y: 0
+    })
+    console.log( 'done', performance.now() - start )
+}
+
+
+render()
+
+
+window.Vector2 = Vector2
+window.Point = Point
+window.render = render
+window.max = max
+window.min = min
+window.clamp = clamp
+window.euclidean = euclidean
+
+window.HeightMap = HeightMap
+
+window.renderer = renderer
+
+window.random = random
+window.base = base
+window.radial = radial
--- src/index.js
+++ src/index.js
@@ -1,189 +1,67 @@
 
-import random from 'lodash.random'
+/**
+ * Heightmap class that handles procedurally generating an x,y point
+ */
+export default class HeightMap {
+    /**
+     * @constructs
+     */
+    constructor() {
+        this.funcs = []
 
-import CONSTANTS from './constants'
-// import Gui from './gui'
-import HeightMap from './heightMap'
-import MapRender from './mapRender'
-import Simplex from './simplex'
-
-import { radial } from './generators'
-import { Point, Vector2, max, min, euclidean, clamp } from './util'
-
-const renderer = new MapRender({
-    style: {
-        zIndex: 10
+        return this
     }
-})
 
-const renderer2 = new MapRender({
-    style: {
-        left: CONSTANTS.WIDTH / window.devicePixelRatio + 'px',
-        zIndex: 10
+    /**
+     * Pushes a generator function and weight to the stack
+     * @param params <Object>
+     *   @param weight <Integer> the weighting to apply to this generator
+     *   @param fn <Func> generator function that returns a value
+     */
+    addFunction( params ) {
+        this.funcs.push( params )
+
+        return this
     }
-})
 
-function lerp( value, min, max ) {
-    return min + value * ( max - min )
-}
-
-
-
-console.log( 'generating simplex' )
-let start = performance.now()
-
-// Simplex functions
-const simplex = new Simplex({
-    min: 0,
-    max: 1,
-    octaves: 4,
-    persistence: .3,
-    frequency: .01
-})
-const perturb = new Simplex({
-    min: -1,
-    max: 1,
-    persistence: .5,
-    frequency: .005
-})
-
-
-// Heightmap function based on base simplex
-const base = new HeightMap()
-    .addFunction({
-        weight: 1,
-        fn: simplex.getValue
-    })
-
-// Maps x, y to 0-.3
-// LEFT base chunk type
-const baseMuted = new HeightMap()
-    .addFunction({
-        weight: 1,
-        fn: ( x, y ) => base.getValue( x, y ) * .3
-    })
-
-
-// RIGHT base chunk type
-const ridged = new HeightMap()
-    .addFunction({
-        weight: 1,
-        fn: base.getValue
-    })
-    .addFunction({
-        weight: 5,
-        fn: function( x, y ) {
-            // Adds the ridges
-            return Math.abs( perturb.getValue( x, y ) )
+    /**
+     * Iterates over a range of values
+     * @param bounds <Rect> bounds to iterate over
+     * @param cb <Func> function to call for each position
+     */
+    iterate( bounds, cb ) {
+        if ( !bounds ) {
+            throw new Error( 'Heightmap::iterate requries a bounding area' )
         }
-    })
-    .addFunction({
-        weight: 5,
-        fn: radial( CONSTANTS.WIDTH / 2 )
-    })
 
+        if ( !cb ) {
+            throw new Error( 'Heightmap::iterate requires an iterator function' )
+        }
 
-const right = new HeightMap()
-    .addFunction({
-        weight: 1,
-        fn: function( x, y ) {
-            // Now handle the seam
-            let seamWidth = CONSTANTS.WIDTH / Math.pow( 2, 2 )
-            let pow = 2
-            let val = 0
-            let i = x % CONSTANTS.WIDTH
-
-            let mapValue = ridged.getValue( x, y )
-
-            if ( i < seamWidth ) {
-                // Heightmap is the chunk to the left in this example
-                val = lerp(
-                    Math.pow( i / seamWidth, pow ),
-                    // i / seamWidth,
-                    baseMuted.getValue( x, y ),      // Values from the left chunk
-                    mapValue                    // mixed with values from this chunk
-                )
-
-                return val
+        for ( let y = bounds.y1; y < bounds.y2; y++ ) {
+            for ( let x = bounds.x1; x < bounds.x2; x++ ) {
+                cb.call( this, this.getValue( x, y ), x, y )
             }
-
-            return mapValue
         }
-    })
 
-const left = new HeightMap()
-    .addFunction({
-        weight: 1,
-        fn: function( x, y ) {
-            // Now handle the seam
-            let seamWidth = CONSTANTS.WIDTH / Math.pow( 2, 2 )
-            let pow = 2
-            let val = 0
-            let i = x % CONSTANTS.WIDTH
+        return this
+    }
 
-            let mapValue = baseMuted.getValue( x, y )
-
-            if ( i > CONSTANTS.WIDTH - seamWidth ) {
-                // Heightmap is the chunk to the left in this example
-                val = lerp(
-                    Math.pow( i / CONSTANTS.WIDTH - seamWidth, pow ),
-                    // i / seamWidth,
-                    ridged.getValue( x, y ),      // Values from the left chunk
-                    mapValue                    // mixed with values from this chunk
-                )
-
-                return val
-            }
-
-            return mapValue
+    getValue = ( x, y ) => {
+        // return this.map[ this.to1d( x, y ) ]
+        if ( !this.funcs.length ) {
+            throw new Error( 'heightmap does not have a generate function' )
         }
-    })
 
+        let value = 0
+        let totalWeight = 0
+        this.funcs.forEach( pass => {
+            totalWeight += pass.weight
+            value += pass.weight * pass.fn( x, y )
+        })
 
-// @TODO sort out the seaming functions
-// should also seam on both sides of the seam, i.e. left chunk permeates into
-// right whilst right goes into left, effectively means that x=1 or right chunk
-// is 50:50, not 100:0 as the current right HeightMap lerp creates
+        // @TODO cache it
+        return value / totalWeight
+    }
 
-
-
-
-console.log( 'done', performance.now() - start )
-
-
-function render() {
-    console.log( 'rendering' )
-    start = performance.now()
-    renderer.clear()
-
-    renderer.render({
-        heightmap: left
-    })
-
-    renderer2.render({
-        heightmap: right,
-        x: 512,
-        y: 0
-    })
-    console.log( 'done', performance.now() - start )
 }
-
-
-render()
-
-
-window.Vector2 = Vector2
-window.Point = Point
-window.render = render
-window.max = max
-window.min = min
-window.clamp = clamp
-window.euclidean = euclidean
-
-window.HeightMap = HeightMap
-
-window.renderer = renderer
-
-window.random = random
-window.base = base
-window.radial = radial
